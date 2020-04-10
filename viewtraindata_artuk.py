4386d76e4b31d6d412b48281b2e03eeece5f3c7f
oncescuandreea <etgeniuand@yahoo.com>
2020-04-10 12:38:56 +0100

    File used for determining the smallest image dimension in the train dataset, used for random/center cropping

---
+++ viewtraindata_artuk.py
@@ -0,0 +1,30 @@
+from collections import Counter
+from pathlib import Path
+
+import numpy as np
+import pandas as pd
+from PIL import Image
+
+
+def get_min_size(csv_contents_path, data_dir):
+    with open(csv_contents_path, "r") as f:
+        csv_contents = pd.read_csv(f)
+    for i, el in enumerate(csv_contents.iloc[:, 1]):
+            img = Image.open(data_dir / f"{el}.jpg")
+            min_size = np.min(img.size)
+            if i == 0:
+                min_size_general = min_size
+            else:
+                if min_size < min_size_general:
+                    min_size_general = min_size
+    return min_size_general
+
+def main():
+    data_dir = Path("/scratch/shared/beegfs/oncescu/pictures_project/artuk/paintings")
+    csv_contents_path = Path("/scratch/shared/beegfs/oncescu/pictures_project/artuk/artuk_lists/artuk_train.csv")
+    min_size_general = get_min_size(csv_contents_path, data_dir)
+    print(f"Minimum pixel size of a painting is {min_size_general}")
+
+
+if __name__ == '__main__':
+    main()
